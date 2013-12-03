d9db2c974fa1e7c7b6d8eae6d7b7ea1528c34443
michael <michael@simplee.com>
2013-12-03 15:30:03 +0200

    reinstall bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cv::Application.config.secret_token = 'bd1e797a5f561837fc5222415b018b800e2211fccf5c6b8b1cfd008b79031c86eac40b6473241e71b648a92fd600ef3329a79cab8641f1e03a95b54e9230c9d2'
